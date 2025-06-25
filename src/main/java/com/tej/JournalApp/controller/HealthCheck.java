82456d8bad77badd2e8f17cd3006417a6025dba2
Tej Praneeth <tejpraneeth190702@gmail.com>
2025-06-25 21:11:44 +0530

    Initial Commit

---
+++ src/main/java/com/tej/JournalApp/controller/HealthCheck.java
@@ -0,0 +1,4 @@
+package com.tej.JournalApp.controller;
+
+public class HealthCheck {
+}
---
+++ src/main/java/com/tej/JournalApp/controller/JournalEntryController.java
@@ -0,0 +1,9 @@
+package com.tej.JournalApp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JournalEntryController {
+
+
+}
