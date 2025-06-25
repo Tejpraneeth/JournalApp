fd242018e7d3400369ca1915378dd190cebbf0b1
Tej Praneeth <tejpraneeth190702@gmail.com>
2025-06-25 21:52:40 +0530

    Creating REST APIs for Journal Entries

---
+++ src/main/java/com/tej/JournalApp/entity/JournalEntry.java
@@ -0,0 +1,15 @@
+package com.tej.JournalApp.entity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class JournalEntry {
+
+
+    private long id;
+    private String title;
+    private String content;
+
+
+}
